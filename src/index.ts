3e53c371153f0a290c09917e2646b682760d9a29
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2022-04-13 20:15:28 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const s = 2;
+console.log(`${s} World`);
+
+export default function two() {
+  return 2;
+}
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,7 @@
+import two from "..";
+
+describe('index', () => {
+    it("should return 2", () => {
+        expect(two()).toBe(2);
+    });
+});
