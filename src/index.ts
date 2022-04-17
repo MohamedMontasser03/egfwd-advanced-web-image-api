754f1a025cc6c783f79e303ecc89a8c22fb7be75
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2022-04-17 14:22:19 +0200

    setup test for healthcheck route

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,5 @@ app.use("/api", routes);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
---
+++ src/tests/routes/indexSpec.ts
@@ -0,0 +1,17 @@
+import supertest from "supertest";
+import app from "../..";
+
+const request = supertest(app);
+console.log(request);
+
+describe("GET /api", () => {
+  it("should return 200 OK", async () => {
+    const res = await request.get("/api");
+    expect(res.status).toBe(200);
+  });
+
+  it("should return 'Server is running'", async () => {
+    const res = await request.get("/api");
+    expect(res.text).toBe("Server is running");
+  });
+});
