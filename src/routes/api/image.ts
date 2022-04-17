6a3fd1205d29f9d1ab7628cfcd6073616e01ae0f
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2022-04-17 14:50:10 +0200

    setup image route

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
-import { getImagePath } from "../../utils/fsUtils";
+import sharp from "sharp";
+import { doesImageExist, getImagePath } from "../../utils/fsUtils";
 
 const image = express.Router();
 
@@ -7,13 +8,26 @@ const image = express.Router();
 image.get("/", async (req: Request, res: Response) => {
   try {
     const { image: imgName } = req.query;
+    const width = 400;
+    const height = 400;
+
     if (!imgName) {
       res.status(400).send({ err: "image query is required" });
     }
     const imgPath = getImagePath(imgName as string);
 
-    // send image
-    res.sendFile(imgPath);
+    // check if image exists
+    if (!doesImageExist(imgPath)) {
+      res.status(404).send({ err: "image not found" });
+    }
+
+    // resize image
+    const image = await sharp(imgPath);
+    const resizedImage = await image.resize(width, height);
+
+    // send resized image
+    res.type("image/jpg");
+    resizedImage.pipe(res);
   } catch (err: unknown) {
     res.status(500).send(err);
   }
